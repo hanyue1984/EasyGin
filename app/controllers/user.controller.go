c35d788fa0c3b38939bafe3587bfdd2642f02798
icecoldmoon <aerith@yeah.net>
2024-03-01 00:00:43 +0800

    url参数接收以及表单参数接收例

--- app/controllers/user.controller.go
+++ app/controllers/user.controller.go
@@ -11,6 +11,11 @@ type UserController struct {
 }
 
 func (u UserController) GetUser(ctx *gin.Context) {
+	//url参数获取
+	//name:= ctx.DefaultQuery("name", "枯藤")
+	//表单获取
+	//types := c.DefaultPostForm("type", "post")
+	//username := c.PostForm("username")
 	user := services.UsersService{}.GetUser(ctx, "12345")
 	if user == nil {
 		ctx.JSON(500, gin.H{
